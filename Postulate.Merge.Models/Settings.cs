230d8b8f9efc7ea933d2c4ff525abd55178b74d2
Adam O'Neil <adamosoftware@gmail.com>
2018-11-19 22:33:14 -0500

    stumped on some library issues

---
+++ Postulate.Merge.Models/Settings.cs
@@ -0,0 +1,39 @@
+﻿using SchemaSync.Library.Models;
+using System.Collections.Generic;
+
+namespace Postulate.Merge.Models
+{
+	public enum TargetConnectionType
+	{
+		/// <summary>
+		/// Literal connection string
+		/// </summary>
+		ConnectionString,
+		/// <summary>
+		/// Reference to a named connection in a config file, example
+		/// DefaultConnection@web.config
+		/// </summary>
+		ConfigFile
+	}
+
+	public class Settings
+	{
+		/// <summary>
+		/// DLL of model classes to merge to the target connection
+		/// </summary>
+		public string SourceAssembly { get; set; }
+
+		public TargetConnectionType TargetConnectionType { get; set; } = TargetConnectionType.ConnectionString;
+
+		/// <summary>
+		/// Connection string of merge target or config name @ config file,
+		/// example: DefaultConnection@web.config
+		/// </summary>
+		public string TargetConnection { get; set; }
+
+		/// <summary>
+		/// Source objects to exclude
+		/// </summary>
+		public HashSet<ExcludeObject> ExcludeObjects { get; set; }
+	}
+}
--- Postulate.Merge.SqlServer/Program.cs
+++ Postulate.Merge.SqlServer/Program.cs
@@ -1,15 +1,78 @@
-﻿using System;
+﻿using JsonSettings;
+using Postulate.Merge.Models;
+using SchemaSync.Library;
+using SchemaSync.Library.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Postulate.Lite;
+using Postulate.Lite.SqlServer;
 
 namespace Postulate.Merge.SqlServer
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			try
+			{
+				string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+				if (args.Length == 0)
+				{
+					CreateEmptySettingsFile(path);					
+					return;
+				}
+
+				string fileName = Path.Combine(path, (args.Length > 1) ? args[1] : "Postulate.Merge.json");
+				var settings = JsonFile.Load<Settings>(fileName);
+
+				var sourceDb = GetAssemblyDb(settings);
+				var targetDb = GetConnectionDb(settings);
+				var diff = SchemaComparison.Execute(sourceDb, targetDb, settings.ExcludeObjects);
+				
+				//diff.SaveScript(new SqlServerSyntax)
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc.Message);
+				Console.ReadLine();
+			}
+		}
+
+		private static Database GetConnectionDb(Settings settings)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static Database GetAssemblyDb(Settings settings)
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void CreateEmptySettingsFile(string path)
+		{
+			var settings = new Settings()
+			{
+				SourceAssembly = "your model class assembly dll",
+				TargetConnection = "target connection string",
+				ExcludeObjects = new HashSet<ExcludeObject>()
+				{
+					new ExcludeObject() { ActionType = ActionType.Create, Name = "Sample", Type = "Table" }
+				}
+			};
+			string fileName = Path.Combine(path, "Postulate.Merge.json");
+			if (File.Exists(fileName))
+			{
+				Console.WriteLine($"Settings file already exists: {fileName}");
+			}
+			else
+			{
+				JsonFile.Save(fileName, settings);
+				Console.WriteLine($"Empty settings file created: {fileName}");
+			}
+			Console.ReadLine();
 		}
 	}
 }
